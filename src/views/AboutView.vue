402c2b0fc8db0e05f366f9820209270b783b7c25
ele-code12 <ma.elena1040@gmail.com>
2024-10-17 22:54:34 -0400

    mi primer commit

---
+++ src/views/AboutView.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="promociones">
+    <h1>PROMOCIONES PIZZA HOUSE</h1>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
